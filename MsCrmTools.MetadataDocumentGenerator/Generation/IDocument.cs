90e6e3cc5c1acade95f882f392f4477c39e700e4
MscrmTools <tanguy92@hotmail.com>
2018-04-24 17:33:46 +0200

    Initial commit

---
+++ MsCrmTools.MetadataDocumentGenerator/Generation/IDocument.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xrm.Sdk;
+using System.ComponentModel;
+
+namespace MsCrmTools.MetadataDocumentGenerator.Generation
+{
+    internal interface IDocument
+    {
+        GenerationSettings Settings { get; set; }
+        BackgroundWorker Worker { set; }
+
+        void Generate(IOrganizationService service);
+    }
+}
---
+++ MsCrmTools.MetadataDocumentGenerator/LanguageCode.cs
@@ -0,0 +1,13 @@
+﻿namespace MsCrmTools.MetadataDocumentGenerator
+{
+    public class LanguageCode
+    {
+        public string Label { get; set; }
+        public int Lcid { get; set; }
+
+        public override string ToString()
+        {
+            return Label;
+        }
+    }
+}
