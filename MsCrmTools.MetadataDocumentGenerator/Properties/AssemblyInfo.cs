12abbe17b6af3e8353f7a2d0e34aef384df1057f
MscrmTools <tanguy92@hotmail.com>
2020-12-09 18:10:05 +0100

    Updating version

--- MsCrmTools.MetadataDocumentGenerator/Properties/AssemblyInfo.cs
+++ MsCrmTools.MetadataDocumentGenerator/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("a8234074-9ff2-4a4c-b580-a2ad4507a116")]
 [assembly: AssemblyVersion("1.2019.5.1")]
-[assembly: AssemblyFileVersion("1.2019.5.1")]
+[assembly: AssemblyFileVersion("1.2020.12.2")]
